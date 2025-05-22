593dca361690fb6ebb17d8d20ce5516c825f7af9
huzaifaghangorcloud <huzaifa@ghangorcloud.com>
2025-05-22 14:52:42 +0500

    working push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,23 @@
 import { Routes } from '@angular/router';
+import { homeRoute } from './component/home/home.routes';
 
 export const routes: Routes = [
+    // {
+    //     path: "",
+    //     redirectTo: "auth/login",
+    //     pathMatch: "full"
+    // },
     {
         path: "",
-        redirectTo: "auth/login",
+        redirectTo: "home",
         pathMatch: "full"
     },
     {
         path: "auth",
         loadChildren: () => import('./component/auth/auth.routes').then(m=> m.auth)
+    },
+    {
+        path: '',
+        children: homeRoute
     }
 ];
