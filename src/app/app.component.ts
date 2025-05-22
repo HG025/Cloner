d303b085ee9ffb462c26c04cbb989d757e22f1cb
huzaifaghangorcloud <huzaifa@ghangorcloud.com>
2025-05-22 14:36:31 +0500

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbNavConfig } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [NgbNavConfig]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang('en')
+    translate.use('en')
+  }
+
+
+  ngOnInit(): void {
+    console.log("app.component.ts file")
+  }
 }
